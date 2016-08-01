15c21a2ad42be9ca11bfddaad8291daca5b2a68f
Charlie Allom <charlie@yelp.com>
2016-08-01 01:12:04 +0100

    get the matching urls already in the db to print the datetime in the buffer

--- python/url_olde.py
+++ python/url_olde.py
@@ -1,13 +1,6 @@
 """
 used sql creation code from https://weechat.org/scripts/source/triggerreply.py.html/
 """
-try:
-    import weechat as w
-    import sqlite3
-    IMPORT_ERR = 0
-except ImportError:
-    IMPORT_ERR = 1
-import os
 
 SCRIPT_NAME = "url_olde"
 SCRIPT_AUTHOR = "Charlie Allom <charlie@evilforbeginners.com"
@@ -15,30 +8,64 @@ SCRIPT_VERSION = "0.1"
 SCRIPT_LICENSE = "Public domain"
 SCRIPT_DESC = "tells you how long ago a URL was first posted."
 
+try:
+    import weechat as w
+    import sqlite3, time
+    IMPORT_ERR = 0
+except ImportError:
+    IMPORT_ERR = 1
+import os
+
+# regex are from urlbar.py, written by xt
+#url_octet = r'(?:2(?:[0-4]\d|5[0-5])|1\d\d|\d{1,2})'
+#url_ipaddr = r'%s(?:\.%s){3}' % (url_octet, url_octet)
+#url_label = r'[0-9a-z][-0-9a-z]*[0-9a-z]?'
+#url_domain = r'%s(?:\.%s)*\.[a-z][-0-9a-z]*[a-z]?' % (url_label, url_label)
+
 def create_db():
     """ create the sqlite database """
     tmpcon = sqlite3.connect(DBFILE)
     cur = tmpcon.cursor()
     cur.execute("CREATE TABLE urls(id INTEGER PRIMARY KEY, uri VARCHAR, date INTEGER);")
-    cur.execute("INSERT INTO urls(uri, date) VALUES ('spodder.com',1)")
+    cur.execute("INSERT INTO urls(uri, date) VALUES ('http://spodder.com',1470006765)")
+    cur.execute("INSERT INTO urls(uri, date) VALUES ('http://moo.com',110006765)")
     tmpcon.commit()
     cur.close()
 
-def 
+
+def search_urls_cb(data, buffer, date, tags, displayed, highlight, prefix, message):
+    """ function for searching for the url """
+    database = sqlite3.connect(DBFILE)
+    database.text_factory = str
+    cursor = database.cursor()
+    for row in cursor.execute("SELECT date,uri from urls WHERE uri LIKE ?", (message,)) :
+        date, uri = row
+        # this comma is special
+        # http://stackoverflow.com/questions/16856647/sqlite3-programmingerror-incorrect-number-of-bindings-supplied-the-current-sta
+        pretty_time = time.ctime(float(str(date)))
+        #w.command(buffer, "/say %s" % pretty_time)
+        w.prnt_date_tags(buffer, 0, 'no_log,notify_none', '%s' % (pretty_time))
+    return w.WEECHAT_RC_OK
+
+def modify_irc_cb(uri, date):
+    """ add the url age to the IRC message """
+    return date
 
 # weechat.register(name, author, version, license, description,
 #                  shutdown_function, charset)
-w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION,
-           SCRIPT_LICENSE, SCRIPT_DESC, '', '')
-
+if w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION,
+           SCRIPT_LICENSE, SCRIPT_DESC, '', ''):
     if IMPORT_ERR:
         w.prnt("", "You need sqlite3 to run this plugin.")
     DBFILE = "%s/olde.sqlite3" % w.info_get("weechat_dir", "")
     if not os.path.isfile(DBFILE):
         create_db()
 
-w.prnt(w.current_buffer(), "feh")
+    # catch urls in buffer and print
+    w.hook_print('', '', '://', 1, 'search_urls_cb', '')
 
+    # test
+    w.prnt(w.current_buffer(), "feh")
 
-def _print_url(data, buffer):
-    return w.WEECHAT_RC_OK
+#def _print_url(data, buffer):
+#    return w.WEECHAT_RC_OK
